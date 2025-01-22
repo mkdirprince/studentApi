da9bac379de21168cd31ff68c1b81d137a1cc79d
mkdirprince <mkdirprince@github.com>
2025-01-22 17:15:42 +0000

    Add a simple server to test config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("yalaaa");
+import express from "express";
+import cors from "cors";
+import config from "./utils/config";
+
+const app = express();
+app.use(cors());
+
+app.listen(config.PORT, () => {
+  console.log(`server running on port ${config.PORT}`);
+});
