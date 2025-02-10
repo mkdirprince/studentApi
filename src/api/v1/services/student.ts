47af023d36d8a61efdefe98d30fec1f837919152
mkdirprince <mkdirprince@github.com>
2025-02-10 09:01:58 +0000

    format code

--- src/api/v1/services/student.ts
+++ src/api/v1/services/student.ts
@@ -4,55 +4,55 @@ import { NewStudent, UpdateStudent } from "../../../types/types";
 import { eq } from "drizzle-orm";
 
 const returnedData = {
-  id: students.id,
-  firstName: students.firstName,
-  lastName: students.lastName,
-  email: students.email,
-  programme: students.programme,
+	id: students.id,
+	firstName: students.firstName,
+	lastName: students.lastName,
+	email: students.email,
+	programme: students.programme,
 };
 
 const getAllStudents = async () => {
-  const allStudents = await db.select(returnedData).from(students);
+	const allStudents = await db.select(returnedData).from(students);
 
-  return allStudents;
+	return allStudents;
 };
 
 const getStudent = async (id: string) => {
-  const [student] = await db
-    .select(returnedData)
-    .from(students)
-    .where(eq(students.id, id));
+	const [student] = await db
+		.select(returnedData)
+		.from(students)
+		.where(eq(students.id, id));
 
-  return student;
+	return student;
 };
 
 const updateStudent = async (id: string, object: UpdateStudent) => {
-  const [updatedStudent] = await db
-    .update(students)
-    .set(object)
-    .where(eq(students.id, id))
-    .returning(returnedData);
+	const [updatedStudent] = await db
+		.update(students)
+		.set(object)
+		.where(eq(students.id, id))
+		.returning(returnedData);
 
-  return updatedStudent;
+	return updatedStudent;
 };
 
 const addStudent = async (object: NewStudent) => {
-  const [savedStudent] = await db
-    .insert(students)
-    .values(object)
-    .returning(returnedData);
+	const [savedStudent] = await db
+		.insert(students)
+		.values(object)
+		.returning(returnedData);
 
-  return savedStudent;
+	return savedStudent;
 };
 
 const removeStudent = async (id: string) => {
-  await db.delete(students).where(eq(students.id, id));
+	await db.delete(students).where(eq(students.id, id));
 };
 
 export default {
-  getAllStudents,
-  getStudent,
-  updateStudent,
-  addStudent,
-  removeStudent,
+	getAllStudents,
+	getStudent,
+	updateStudent,
+	addStudent,
+	removeStudent,
 };
