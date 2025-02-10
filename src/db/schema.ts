68a21ec768763820fff97cc8cb24d9d921b1eca1
mkdirprince <mkdirprince@github.com>
2025-02-10 09:00:50 +0000

    modify schema to remove passwordHash

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,16 +2,16 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 import { v1 as uuid7 } from "uuid";
 
 const createPrefix = (table: string) => {
-  return `${table}_${uuid7()}`;
+	return `${table}_${uuid7()}`;
 };
 
 export const students = pgTable("students", {
-  id: text()
-    .notNull()
-    .$defaultFn(() => createPrefix("student")),
-  firstName: text().notNull(),
-  lastName: text().notNull(),
-  email: text().notNull().unique(),
-  password: text().notNull(),
-  programme: text().notNull(),
+	id: text()
+		.notNull()
+		.$defaultFn(() => createPrefix("student")),
+	firstName: text().notNull(),
+	lastName: text().notNull(),
+	email: text().notNull().unique(),
+	password: text().notNull(),
+	programme: text().notNull(),
 });
